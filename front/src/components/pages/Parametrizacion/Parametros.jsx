bee9ab1acc525a8ed91c664dc6613792c3c68443
Felipe <calderonfelipe017gmail.com>
2023-10-27 16:29:56 -0500

    Fin Insert

--- front/src/components/pages/Parametrizacion/Parametros.jsx
+++ front/src/components/pages/Parametrizacion/Parametros.jsx
@@ -3,6 +3,26 @@ import { Button, Modal } from "react-bootstrap";
 
 const Parametros = () => {
   const [show, setShow] = useState(false);
+  const [maxIdParametros, setmaxIdParametros] = useState([]);
+  const [Parametros, setParametros] = useState([]);
+  const [insercionParametros, setIncersionParametros] = useState(false);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:3900/api/parametro/listar")
+      .then((response) => {
+        return response.json();
+      })
+      .then((doc) => {
+        setParametros(doc);
+      });
+    fetch("http://127.0.0.1:3900/api/parametro/maximo/id")
+      .then((response) => {
+        return response.json();
+      })
+      .then((doc) => {
+        setmaxIdParametros(doc.maximo);
+      });
+  }, []);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -26,10 +46,11 @@ const Parametros = () => {
               className="table table-bordered"
               id="dataTable"
               width="100%"
-              cellspacing="0"
+              cellSpacing="0"
             >
               <thead>
                 <tr>
+                  <th>#</th>
                   <th>Nombre</th>
                   <th>Descripción</th>
                   <th>Valor</th>
@@ -39,16 +60,21 @@ const Parametros = () => {
               </thead>
 
               <tbody>
-                <tr>
-                  <td>Autenticación</td>
-                  <td>Autenticación</td>
-                  <td>false</td>
-                  <td>ACTIVO</td>
-                  <td className="text-center">
-                    {" "}
-                    <button className="btn btn-success fa fa-pencil "></button>
-                  </td>
-                </tr>
+                {Parametros.map((res) => {
+                  return (
+                    <tr key={res.id}>
+                      <td>{res.id}</td>
+                      <td>{res.Nombre}</td>
+                      <td>{res.Descripcion}</td>
+                      <td>{res.Valor}</td>
+                      <td>{res.Estado}</td>
+                      <td className="text-center">
+                        {" "}
+                        <button className="btn btn-success fa fa-pencil "></button>
+                      </td>
+                    </tr>
+                  );
+                })}
               </tbody>
             </table>
           </div>
@@ -57,50 +83,90 @@ const Parametros = () => {
           <Modal.Header className="bg-light" closeButton>
             <Modal.Title>Agregar Parámetro</Modal.Title>
           </Modal.Header>
-          <form>
+          <form
+            method="post"
+            onSubmit={(e) => {
+              e.preventDefault();
+              let id = document.querySelector("#idParametro");
+              let nombre = document.querySelector("#nombreParametro");
+              let descripcion = document.querySelector("#descripcionParametro");
+              let estado = document.querySelector("#estadoParametro");
+              let valor = document.querySelector("#valorParametro");
+              fetch("http://127.0.0.1:3900/api/parametro/agregar", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/x-www-form-urlencoded",
+                },
+                body: `id=${id.value}&Nombre=${nombre.value}&Descripcion=${descripcion.value}&Valor=${valor.value}&Estado=${estado.value}`,
+              })
+                .then((response) => {
+                  return response.json();
+                })
+                .then((res) => {
+                  console.log(res);
+                  setIncersionParametros(true);
+                });
+            }}
+          >
             <Modal.Body>
-              <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">
+              <div className="mb-3">
+                <label htmlFor="exampleInputEmail1" className="form-label">
+                  id <b className="text-danger">*</b>
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="idParametro"
+                  disabled
+                  value={maxIdParametros + 1}
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="exampleInputEmail1" className="form-label">
                   Nombre <b className="text-danger">*</b>
                 </label>
                 <input
                   type="text"
                   className="form-control"
-                  id="nombreIndicadorPlanDesarrollo"
+                  id="nombreParametro"
                   aria-describedby="emailHelp"
                 />
               </div>
               <div className="mb-3">
-                <label for="exampleInputPassword1" className="form-label">
+                <label htmlFor="exampleInputPassword1" className="form-label">
                   Descripción
                 </label>
                 <textarea
                   className="form-control"
                   name=""
-                  id="descripcionIndicadorPlanDesarrollo"
+                  id="descripcionParametro"
                   rows="5"
                   style={{ resize: "none" }}
                 ></textarea>
               </div>
 
               <div className="mb-3">
-                <label for="exampleInputEmail1" class="form-label">
+                <label htmlFor="exampleInputEmail1" className="form-label">
                   Valor
                 </label>
                 <input
                   type="text"
                   className="form-control"
-                  id="nombreIndicadorPlanDesarrollo"
+                  id="valorParametro"
                   aria-describedby="emailHelp"
                 />
               </div>
               <div className="mb-3">
-                <label for="" className="form-label">
+                <label htmlFor="" className="form-label">
                   Estado
                 </label>
-                <select class="form-select" aria-label="Default select example">
-                  <option value="1">Activo</option>
-                  <option value="2">Inactivo</option>
+                <select
+                  className="form-select"
+                  aria-label="Default select example"
+                  id="estadoParametro"
+                >
+                  <option value="Activo">Activo</option>
+                  <option value="Inactivo">Inactivo</option>
                 </select>
               </div>
             </Modal.Body>
